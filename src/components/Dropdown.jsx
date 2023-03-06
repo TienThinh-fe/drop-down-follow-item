bbb4acc0d7f2456103b5c954421f7eed78cc1bab
Hoang Tien Thinh <85208341+TienThinh-fe@users.noreply.github.com>
2023-03-06 03:26:26 +0000

    feat: style for container

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,9 +1,28 @@
+import styled from "styled-components"
+
 import { ReactComponent as ArrowIcon } from "../assets/arrow.svg"
 
+const DropdownContainer = styled.div`
+  width: 160px;
+  height: 40px;
+  background-color: hsla(0, 0%, 100%, 0.03);
+  border-radius: 8px;
+  display: flex;
+  align-items: center
+  cursor: pointer;
+`;
+
+const Title = styled.span`
+  margin: 0 16px
+`
+
 const Dropdown = () => {
-  return <div>Dropdown
+  return <DropdownContainer>
+    <Title>
+      Dropdown
+    </Title>
     <ArrowIcon />
-    </div>
+    </DropdownContainer>
 }
 
 export default Dropdown
